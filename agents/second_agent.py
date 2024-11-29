7b8e2ee9c550c44a931fe54804a37c1d9709912e
Leonardo Martinez <leonardo.martinez@mail.mcgill.ca>
2024-11-29 17:46:38 -0500

    fixed merge issue or something, true MTCTS implemented

--- agents/second_agent.py
+++ agents/second_agent.py
@@ -16,25 +16,6 @@ class SecondAgent(Agent):
         self.name = "SecondAgent"
 
     @staticmethod
-    def utility(board, player, opponent):
-        #tells us which player is currently winning
-        is_endgame, p1_score, p2_score = check_endgame(board, player, opponent)
-        if player == 1:
-            if p1_score > p2_score:
-                return 1
-            elif p1_score < p2_score:
-                return -1
-            else:
-                return 0
-        else:
-            if p2_score > p1_score:
-                return 1
-            elif p2_score < p1_score:
-                return -1
-            else:
-                return 0
-
-    @staticmethod
     def promising_moves(valid_moves, board, player, opp):
         #returns up to 5 of the best moves
         #currently based on the utility of such moves
@@ -71,34 +52,39 @@ class SecondAgent(Agent):
         for move in valid_moves:
             simb = deepcopy(chess_board)#simulated board
             is_endgame = False
-            score = 0
             execute_move(simb,move,player)
             is_endgame,p1,p2 = check_endgame(simb, player, opponent)
             simplayer = player
+            avgscore = 0
             time_taken = time.time() - start_time
 
             if (time_taken > 1.9):#to avoid exceeding time
                 break
-            counter  = 100;#only go down to specified depth
-            while(not is_endgame and counter > 0):
-                #simulate 5 games per valid move
-                tmp = random_move(simb, simplayer)
-                if (tmp != None):
-                    execute_move(simb,tmp,simplayer)
-                    is_endgame,p1,p2 = check_endgame(simb, player, opponent)
-                if (simplayer == player):
-                    simplayer = (3 - player)
-                else:
-                    simplayer = player
-                counter -= 1 #decrement counter
 
-            if(player == 1):#we are player 1
-                score = p1
-            else:#we are player 2
-                score = p2
+            for i in range(5):
+                #simulate 5 games per chosen moves
+                counter  = 100;#only go down to specified depth
+                simb2 = deepcopy(simb)#simulated board
+                while(not is_endgame and counter > 0):
+                    tmp = random_move(simb2, simplayer)
+                    if (tmp != None):
+                        execute_move(simb2,tmp,simplayer)
+                        is_endgame,p1,p2 = check_endgame(simb2, player, opponent)
+                    if (simplayer == player):
+                        simplayer = (3 - player)
+                    else:
+                        simplayer = player
+                    counter -= 1 #decrement counter
 
-            if (score > maxscore):
-                maxscore = score
+                if(player == 1):#we are player 1
+                    avgscore += p1
+                else:#we are player 2
+                    avgscore += p2
+
+            #compute avg score
+            avgscore /= 5
+            if (avgscore > maxscore):
+                maxscore = avgscore
                 bestmove = move
 
         time_taken = time.time() - start_time
