150a6b3ff995b505e5388e74ed2555531ec41c63
Leonardo Martinez <leonardo.martinez@mail.mcgill.ca>
2024-11-29 16:29:01 -0500

    added another agent for further testing with alphabeta pruning

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,5 +2,6 @@ from .random_agent import RandomAgent
 from .human_agent import HumanAgent
 from .student_agent import StudentAgent
 from .alphabeta_agent import AlphabetaAgent
+from .alpha2_agent import Alpha2Agent
 from .second_agent import SecondAgent
 from .gpt_greedy_corners_agent import StudentAgent
