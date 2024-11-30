9da11933a8ad8b7e95daec47bde989233554b433
AgustinSanto <124857618+AgustinSanto@users.noreply.github.com>
2024-11-30 16:21:53 -0500

    Update alpha2_agent.py
    changed depth

--- agents/alpha2_agent.py
+++ agents/alpha2_agent.py
@@ -142,7 +142,7 @@ class Alpha2Agent(Agent):
 
     def step(self, chess_board, player, opponent):
         start_time = time.time()
-        depth = 3
+        depth = 4
         valid_moves = get_valid_moves(chess_board, player)
 
         if not valid_moves:
