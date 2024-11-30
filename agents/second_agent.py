bb8c0248a218d95a291ba0e380bebef05cb0e73b
Leonardo Martinez <leonardo.martinez@mail.mcgill.ca>
2024-11-30 14:44:46 -0500

    second agent now behaves diff when more than 50% of the board is filled

--- agents/second_agent.py
+++ agents/second_agent.py
@@ -14,7 +14,7 @@ class SecondAgent(Agent):
     def __init__(self):
         super(SecondAgent, self).__init__()
         self.name = "SecondAgent"
-        self.count = 3 #this is to update the board every n moves
+        #self.count = 3 #this is to update the board every n moves
         self.boardfill = 0
         self.boardsize = -1
 
@@ -30,17 +30,10 @@ class SecondAgent(Agent):
 
 
     @staticmethod
-    def promising_moves(valid_moves, board, player, opp):
+    def promising_moves(valid_moves, board, player, opp, key):
         #returns up to 5 of the best moves
         #currently based on the utility of such moves
         scores = []
-        def find_top_n(nums, n):
-            #Finds the n highest values in a list.
-            # Create a heap of size n, with the smallest element at the top
-            heap = []
-            for tup in nums:
-                heapq.heappush(heap, tup)
-            return [tup[0] for tup in heapq.nlargest(n,heap, lambda x: x[1])]
         #compute the scores
         for move in valid_moves:
             simb = deepcopy(board)
@@ -50,24 +43,28 @@ class SecondAgent(Agent):
                 scores.append((move,p1))
             else:
                 scores.append((move,p2))
-        return find_top_n(scores, 5)
+        return key(scores, 5)
 
 
     def step(self, chess_board, player, opponent):
         start_time = time.time()
-        self.count -= 1
-        if self.count == 0:
-            self.boardfill = SecondAgent.updatefill(self,chess_board,player)
-            print("board filled at ", self.boardfill, "%")
-            self.count = 3
+        #self.count -= 1
         valid_moves = get_valid_moves(chess_board, player)
         if len(valid_moves) == 0:
             return None
-        #print("we have ",len(valid_moves), "valid moves")
+        #if self.count == 0:
+        self.boardfill = SecondAgent.updatefill(self,chess_board,player)
+        #print("board filled at ", self.boardfill, "%")
+        #self.count = 3
         maxscore = 0
         bestmove = None
+        
+        if self.boardfill < 0.5:
+            key = find_low_n
+        else:
+            key = find_top_n
 
-        valid_moves = SecondAgent.promising_moves(valid_moves, chess_board, player, opponent)
+        valid_moves = SecondAgent.promising_moves(valid_moves, chess_board, player, opponent, key)
         for move in valid_moves:
             simb = deepcopy(chess_board)#simulated board
             is_endgame = False
@@ -110,3 +107,18 @@ class SecondAgent(Agent):
         print("My AI's turn took ", time_taken, "seconds.")
         return bestmove
 
+def find_top_n(nums, n):
+    #Finds the n highest values in a list.
+    #Create a heap of size n, with the smallest element at the top
+    heap = []
+    for tup in nums:
+        heapq.heappush(heap, tup)
+    return [tup[0] for tup in heapq.nlargest(n,heap, lambda x: x[1])]
+
+def find_low_n(nums, n):
+    #Finds the n highest values in a list.
+    #Create a heap of size n, with the smallest element at the top
+    heap = []
+    for tup in nums:
+        heapq.heappush(heap, tup)
+    return [tup[0] for tup in heapq.nsmallest(n,heap, lambda x: x[1])]
