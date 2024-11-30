a4acb68a1201a2fc819acfc4fcddfba008bc5cc6
AgustinSanto <124857618+AgustinSanto@users.noreply.github.com>
2024-11-30 17:58:16 -0500

    Update alpha2_agent.py

--- agents/alpha2_agent.py
+++ agents/alpha2_agent.py
@@ -27,14 +27,14 @@ class Alpha2Agent(Agent):
 
 
     def updatefill(self, board, player):
-    if self.boardsize == -1:
-        self.boardsize = len(board) ** 2 #getboardsize
-    count = 0
-    for row in board:
-        for entry in row:
-            if entry != 0:
-                count += 1
-    return count / self.boardsize
+        if self.boardsize == -1:
+            self.boardsize = len(board) ** 2 #getboardsize
+        count = 0
+        for row in board:
+            for entry in row:
+                if entry != 0:
+                    count += 1
+        return count / self.boardsize
 
 
     def initavoid(self, board):
