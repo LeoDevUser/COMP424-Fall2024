9ca464cca9f77734f618310eb8f2835bb6f0e407
AgustinSanto <124857618+AgustinSanto@users.noreply.github.com>
2024-12-01 13:29:58 -0500

    Update third_agent.py
    changed time limit

--- agents/third_agent.py
+++ agents/third_agent.py
@@ -200,7 +200,7 @@ class ThirdAgent(Agent):
     
         for move in valid_moves:
             time_taken = time.time() - start_time
-            if time_taken > 1.99:
+            if time_taken > 1.9:
                 break
             board_copy = deepcopy(chess_board)
             execute_move(board_copy, move, player)
